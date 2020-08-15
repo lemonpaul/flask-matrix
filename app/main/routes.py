586ed169a5c6ef1fca44fc2f0893e30bdbf965dc
lemonpaul <leamonpaul@yandex.ru>
2020-08-15 15:53:36 +0300

    Add theory section and MathJax

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/explore')
+def explore():
+    return render_template('explore.html', title='Theory')
