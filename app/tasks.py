92b96975998afcf5a34c2a316a0b46932ecd77e5
lemonpaul <leamonpaul@yandex.ru>
2020-12-04 01:19:12 +0300

    New init function.

--- app/tasks.py
+++ app/tasks.py
@@ -12,29 +12,51 @@ def init_matrices(height=3, width=3):
             for body in range(0, 1 << w*h):
                 matrix = Matrix(width=w, height=h, body=body)
                 db.session.add(matrix)
-                matrix.l_class = L_class()
-                matrix.r_class = R_class()
-                matrix.h_class = H_class()
-
-                for class_matrix in Matrix.query.all():
-                    if matrix == class_matrix:
-                        continue
-                    if matrix.row_space() == class_matrix.row_space():
-                        matrix.l_class = class_matrix.l_class
-                    if matrix.column_space() == class_matrix.column_space():
-                        matrix.r_class = class_matrix.r_class
-                    if matrix.row_space() == class_matrix.row_space() and matrix.column_space() == class_matrix.column_space():
-                        matrix.h_class = class_matrix.h_class
-                        break
 
     for matrix in Matrix.query.all():
+        for h_class in H_class.query.all():
+            class_matrix = h_class.matrices[0]
+            if matrix.row_space() == class_matrix.row_space() and matrix.column_space() == class_matrix.column_space():
+                class_matrix.h_class.matrices.append(matrix)
+                break
+        else:
+            matrix.h_class = H_class()
+
+    for h_class in H_class.query.all():
+        matrix = h_class.matrices[0]
+        for l_class in L_class.query.all():
+            class_matrix = l_class.matrices[0]
+            if matrix.row_space() == class_matrix.row_space():
+                class_matrix.l_class.matrices.extend(h_class.matrices)
+                break
+        else:
+            class_ = L_class()
+            class_.matrices.extend(h_class.matrices)
+            db.session.add(class_)
+
+    for h_class in H_class.query.all():
+        matrix = h_class.matrices[0]
+        for r_class in R_class.query.all():
+            class_matrix = r_class.matrices[0]
+            if matrix.column_space() == class_matrix.column_space():
+                class_matrix.r_class.matrices.extend(h_class.matrices)
+                break
+        else:
+            class_ = R_class()
+            class_.matrices.extend(h_class.matrices)
+            db.session.add(class_)
+
+    for l_class in L_class.query.all():
+        matrix = l_class.matrices[0]
         for d_class in D_class.query.all():
             class_matrix = d_class.matrices[0]
             if set(matrix.r_class.matrices) & set(class_matrix.l_class.matrices):
-                matrix.d_class = class_matrix.d_class
+                class_matrix.d_class.matrices.extend(l_class.matrices)
                 break
         else:
-            matrix.d_class = D_class()
+            class_ = D_class()
+            class_.matrices.extend(l_class.matrices)
+            db.session.add(class_)
 
     db.session.commit()
 
@@ -54,5 +76,4 @@ def init(height=3, width=3):
 
     start_time = time.time()
     init_matrices(height, width)
-    #  init_d_classes()
     print(f'Elapsed time: {(time.time() - start_time):.2f}')
