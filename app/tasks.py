f7a3c73bab634a7152307e64383c3c1e452e7c96
lemonpaul <leamonpaul@yandex.ru>
2020-12-08 20:03:50 +0300

    Add batches

--- app/tasks.py
+++ app/tasks.py
@@ -6,7 +6,7 @@ app = create_app()
 app.app_context().push()
 
 
-def init_matrices(height=3, width=3):
+def init_matrices(height=3, width=3, max_batch_size=512):
     for h in range(1, height+1):
         for w in range(1, width + 1):
             for body in range(0, 1 << w*h):
@@ -15,14 +15,44 @@ def init_matrices(height=3, width=3):
 
     for h in range(1, height+1):
         for w in range(1, width + 1):
-            for matrix in Matrix.query.filter(width == w, height == h).all():
-                for h_class in H_class.query.join(Matrix).filter(Matrix.width == w, Matrix.height == h).all():
-                    class_matrix = h_class.matrices[0]
-                    if matrix.row_space() == class_matrix.row_space() and matrix.column_space() == class_matrix.column_space():
-                        class_matrix.h_class.matrices.append(matrix)
-                        break
-                else:
-                    matrix.h_class = H_class()
+            matrices = Matrix.query.filter(Matrix.width == w, Matrix.height == h).all()
+            matrix_length = matrices.__len__()
+            batch_size = min(matrix_length, max_batch_size)
+            batch_length = int(matrix_length / batch_size)
+            print(f'{w}x{h} matrices. {batch_length} batches.')
+            batches = []
+            for n_batch in range(0, batch_length):
+                print(f'Fill batch {n_batch+1}/{batch_length}...')
+                batches.append([])
+                for matrix in matrices[n_batch*batch_size:(n_batch+1)*batch_size]:
+                    for h_class in batches[n_batch]:
+                        class_matrix = h_class.matrices[0]
+                        if matrix.row_space() == class_matrix.row_space() and matrix.column_space() == class_matrix.column_space():
+                            h_class.matrices.append(matrix)
+                            break
+                    else:
+                        matrix.h_class = H_class()
+                        batches[n_batch].append(matrix.h_class)
+
+            all_h_classes = []
+            if batch_length > 1:
+                n_batch = 1
+                for batch in batches:
+                    print(f'Process batch {n_batch}/{batch_length}, {len(batch)} H-classes...')
+
+                    for batch_h_class in batch:
+                        matrix = batch_h_class.matrices[0]
+                        for h_class in all_h_classes:
+                            class_matrix = h_class.matrices[0]
+                            if matrix.row_space() == class_matrix.row_space() and matrix.column_space() == class_matrix.column_space():
+                                h_class.matrices.extend(list(batch_h_class.matrices))
+                                all_h_classes.append(h_class)
+                                db.session.expunge(batch_h_class)
+                                break
+                        else:
+                            all_h_classes.append(batch_h_class)
+
+                    n_batch += 1
 
     for h_class in H_class.query.all():
         matrix = h_class.matrices[0]
@@ -60,7 +90,7 @@ def init_matrices(height=3, width=3):
     db.session.commit()
 
 
-def init(height=3, width=3):
+def init(height=3, width=3, max_batch_size=512):
     Matrix.query.delete()
     H_class.query.delete()
     L_class.query.delete()
@@ -73,6 +103,6 @@ def init(height=3, width=3):
     db.session.execute("ALTER SEQUENCE d_class_id_seq RESTART WITH 1")
     db.session.commit()
 
-    start_time = time.time()
-    init_matrices(height, width)
-    print(f'Elapsed time: {(time.time() - start_time):.2f}')
+    start = time.time()
+    init_matrices(height, width, max_batch_size)
+    return time.time() - start
